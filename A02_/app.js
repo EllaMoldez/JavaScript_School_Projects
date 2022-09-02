61389a8862dc3cb80c0f9fb5395ea2394e8cc9a8
EllaMoldez <90626155+EllaMoldez@users.noreply.github.com>
2022-09-01 22:01:43 -0300

    Upload

---
+++ A02_/app.js
@@ -0,0 +1,12 @@
+/*
+ * Name: Ella Moldez
+ * Title: JavaScript Programming - Assignment 2
+ * Program: ITPA 2021
+ * Date: 10/06/2021
+ */
+
+let firstName = prompt("Please enter your first name");
+let lastName = prompt("Please enter your last name");
+alert(`${firstName} ${lastName}. I have several prompts for your to collect some data. 
+Please be patient and complete them all.`);
+
